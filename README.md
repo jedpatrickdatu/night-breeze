b9af6f8bc1b0d8e23cfe72848e2df295c988a1e8
Jed Patrick Datu <jedpatrickdatu@gmail.com>
2019-12-15 10:30:07 +0800

    Implements the above-the-fold part of the homepage.

---
+++ README.md
@@ -0,0 +1,22 @@
+# night-breeze
+
+> A website for Joy Justo&#39;s ladies salon.
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm run install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="content">
+      <div class="content-inner">
+        <img class="logo" src="../assets/images/Logo.png" />
+        <h1 class="header">
+          NIGHT BREEZE
+        </h1>
+        <h2 class="subheader">
+          LADIES SALON
+        </h2>
+        <h4 class="slogan">
+          Let us help you find your new style.
+        </h4>
+        <button class="book-cta">
+          Book an Appointment
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import './index.scss'
+
+export default {
+  components: {}
+}
+</script>
